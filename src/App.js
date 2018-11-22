6d42e47857c00d09e701393b0ae06b687b1eff24
natacha romano <romano.n@hotmail.com>
2018-11-22 20:53:44 -0200

    creating the states and render

--- src/App.js
+++ src/App.js
@@ -8,28 +8,28 @@ import Library from './Library';
 class BooksApp extends React.Component {
   state = {
     books: [],
-    reading: [],
-    wantRead: [],
-    dontRead:[]
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
   }
 
   showBookShelf() {
     BooksAPI.getAll()
-    .then((books) => {
-      this.setState({
-        books: books,
-        reading: books.filter((book) => book.shelf === 'reading'),
-        wantRead: books.filter((book) => book.shelf === 'wantRead'),
-        dontRead: books.filter((book) => book.shelf === 'dontRead')
+      .then((books) => {
+        this.setState({
+          books: books,
+          currentlyReading: books.filter((book) => book.shelf === 'currentlyReading'),
+          wantToRead: books.filter((book) => book.shelf === 'wantToRead'),
+          read: books.filter((book) => book.shelf === 'read')
+        })
       })
-    })
   }
 
   updateBookShelf = (book, shelf) => {
     BooksAPI.update(book, shelf)
-    .then((books) => {
-      this.showBookShelf()
-    })
+      .then((books) => {
+        this.showBookShelf()
+      })
   }
 
   componentDidMount() {
@@ -41,12 +41,12 @@ class BooksApp extends React.Component {
       <div className="app">
         <Route exact path={process.env.PUBLIC_URL + "/"} render={() => (
           <Library
-            reading={this.state.reading}
-            wantRead={this.state.wantRead}
-            dontRead={this.state.dontRead} />
+            currentlyReading={this.state.currentlyReading}
+            wantToRead={this.state.wantToRead}
+            read={this.state.read} />
         )} />
         <Route exact path={process.env.PUBLIC_URL + "/search"} render={() => (
-          <SearchBook/>
+          <SearchBook />
         )} />
       </div>
     )
