b394229630be920b3ab28aab700ad1ee49e4eaef
natacha romano <romano.n@hotmail.com>
2018-11-23 19:45:32 -0200

    Updates

---
+++ precache-manifest.e697f67aa7b107466b8b2d600b06674d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9afc09a080ebf76f72d2f7889249877a",
+    "url": "/myreads-react/static/media/loader.9afc09a0.gif"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-react/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-react/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-react/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3923c7c42fa552ff5f3a",
+    "url": "/myreads-react/static/js/runtime~main.3923c7c4.js"
+  },
+  {
+    "revision": "b398a1bc2dac88901419",
+    "url": "/myreads-react/static/js/main.b398a1bc.chunk.js"
+  },
+  {
+    "revision": "d9bdd9a469b86580d22d",
+    "url": "/myreads-react/static/js/1.d9bdd9a4.chunk.js"
+  },
+  {
+    "revision": "b398a1bc2dac88901419",
+    "url": "/myreads-react/static/css/main.5c9fd9e0.chunk.css"
+  },
+  {
+    "revision": "83cbb520363e0b54ed424577ef9c10d3",
+    "url": "/myreads-react/index.html"
+  }
+];
